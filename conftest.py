1752c748e3cd749354c87cbace3e2a2098c2a06d
satee143 <satee143@gmail.com>
2019-04-13 22:11:26 +0530

    changes done the driver

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ def pytest_addoption(parser):
 def Setup(request):
      browser=request.config.getoption('--browser')
      if browser=='chrome':
-          driver=webdriver.Chrome(executable_path='C:/Users/Dusa/PycharmProjects/zoopla.co.uk/Drivers/chromedriver.exe')
+          driver=webdriver.Chrome(executable_path='../Drivers/chromedriver.exe')
      elif browser=='firefox':
           driver=webdriver.Firefox(executable_path='C:/Users/Dusa/PycharmProjects/zoopla.co.uk/Drivers/geckodriver.exe')
      request.cls.driver=driver
